048af08d19e2934ba1c9a4815bdfab48459e41c1
Ryan <ryanfang@foxmail.com>
2026-04-14 21:56:13 -0400

    Add MainMenu/GameScene, SceneChanger
    Add new MainMenu.unity and GameScene.unity scenes (SampleScene removed). Add SceneChanger.cs manager and hook MainMenu button to SceneChanger.LoadScene for loading Scenes/GameScene.

---
+++ Assets/Scripts/Managers/SceneChanger.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public static SceneChanger Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
